c3e6342f658e8b96b0d78e37eb589ccd3f28f889
Rodrigo Quaresma <rodrigoquaresma@gmail.com>
2022-05-20 13:38:34 -0300

    start

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
+import React from 'react';
+import logo from './icons/check-solid.svg';
+import Navbar from './components/navbar.js';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -15,9 +17,10 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Reactddd
         </a>
       </header>
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Navbar = () => {
+  return (
+    <section className="navbar">
+    Plá
+    </section>
+  )
+};
+
+export default Navbar;
